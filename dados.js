8a3e006c6680c041e8ce5ce1ec9adf57f9ef417a
Deivid de Souza <101485535+deividfront-end@users.noreply.github.com>
2024-09-06 21:32:53 -0300

    Add files via upload

---
+++ dados.js
@@ -0,0 +1,22 @@
+let dados = [
+    
+    {
+        titulo: "Bolo de Chocolate",
+        preparo: "Bata no liquidificador 2 ovos, 1 xícara de açúcar, 1 xícara de óleo, 1 xícara de leite, 2 xícaras de farinha de trigo e 1 colher de sopa de fermento. Asse por 30 minutos.Bata no liquidificador 2 ovos, 1 xícara de açúcar, 1 xícara de óleo, 1 xícara de leite, 2 xícaras de farinha de trigo, 1 xícara de chocolate em pó e 1 colher de sopa de fermento. Asse por 35 minutos.",
+        link: "https://pt.wikipedia.org/wiki/Bolo_de_chocolate",
+        tags: " Chocolate  chocolate"
+    },
+    {
+        titulo: "Bolo de Cenoura",
+        preparo: "Bata no liquidificador 2 cenouras raladas, 1 xícara de óleo, 4 ovos, 2 xícaras de açúcar, 2 xícaras de farinha de trigo e 1 colher de sopa de fermento. Asse por 40 minutos.",
+        link: "https://pt.wikipedia.org/wiki/Bolo_de_cenoura",
+        tags: "Cenoura cenoura"
+    },
+
+    {
+        titulo: "Bolo de Laranja",
+        preparo: "Bata no liquidificador: 2 laranjas com casca (sem sementes) picadas, 1 xícara de óleo, 4 ovos, 2 xícaras de açúcar, 2 xícaras de farinha de trigo e 1 colher de sopa de fermento em pó. Asse em forma untada e enfarinhada por cerca de 40 minutos.",
+        link: "https://primeirahora.com.br/7-curiosidades-sobre-o-bolo-de-laranja-que-voce-precisa-conhecer/#:~:text=A%20origem%20da%20iguaria%20pode,a%20ideia%20para%20o%20Brasil.",
+        tags: "laranja  Laranja"
+    }
+];
